5f52c9b81d7066e08b54fdfc66164560cd2fc94d
shinnrai <ofedorov@student.42.us.org>
2017-01-18 21:57:07 -0800

    libft.h edit: ft_atoi returns long long int

--- includes/libft.h
+++ includes/libft.h
@@ -40,7 +40,7 @@ char			*ft_strstr(const char *big, const char *little);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
-int				ft_atoi(const char *str);
+long long		ft_atoi(const char *str);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
